701eebdb2281190e612ae8b83c7f52253a87c1d4
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-09 15:16:49 -0800

    task 12

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            str = 'FizzBuzz '
+            for x in str:
+                print('{}'.format(x), end='')
+        if i % 3 == 0:
+            str = 'Fizz '
+            for y in str:
+                print('{}'.format(y), end='')
+        elif i % 5 == 0:
+            str = 'Buzz '
+            for z in str:
+                print('{}'.format(z), end='')
+        else:
+            print('{}'.format(i), end=' ')
