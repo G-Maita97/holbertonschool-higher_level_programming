2f13b9b9ce2c3e22f644a3ee0a29fba017214464
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-11 18:19:22 -0800

    task2

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    pre_num = len(sys.argv) - 1
+    if len(sys.argv) == 1 or len(sys.argv) > 2:
+        text_first = "arguments"
+        if len(sys.argv) == 1:
+            point = '.'
+        else:
+            point = ':'
+    elif len(sys.argv) == 2:
+        text_first = "argument"
+        point = ':'
+    
+    print('{} {}{}'.format(pre_num, text_first, point), end='\n')
+
+    for i in range(pre_num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
