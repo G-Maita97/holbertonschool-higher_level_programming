3bd5c6327562d57ad333a387e3bf8b0a84e477de
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-14 18:05:29 -0800

    task4

---
+++ python-more_data_structures/4-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+only_diff_elements = __import__('4-only_diff_elements').only_diff_elements
+
+set_1 = { "Python", "C", "Javascript" }
+set_2 = { "Bash", "C", "Ruby", "Perl" }
+od_set = only_diff_elements(set_1, set_2)
+print(sorted(list(od_set)))
