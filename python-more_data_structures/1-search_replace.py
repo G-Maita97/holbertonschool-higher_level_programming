e0785e2e0ab586b76d2d8a6323f49689bdcc1880
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-14 18:32:43 -0800

    task1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    new_list = [x if x != search else replace for x in my_list]
-    return(new_list)
-
-if __name__ == "__main__":
-    search_replace()
+    new_list = my_list.copy()
+    for i in range(0, len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
+    return new_list
 
