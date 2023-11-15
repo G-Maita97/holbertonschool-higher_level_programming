171223f67aad32286098712369f38e4e07205067
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-14 18:15:18 -0800

    task4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    set_1.update(set_2)
-    removedValue = set_1.discard('C')
-    return (set_1)
+    symmetric_difference = set_1 ^ set_2
+    return (symmetric_difference)
