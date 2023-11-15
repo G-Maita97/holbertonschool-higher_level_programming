923b705c20b1d2bd0227e31ff5560d2f7c530561
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-14 18:02:11 -0800

    task1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,3 +3,7 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     new_list = [x if x != search else replace for x in my_list]
     return(new_list)
+
+if __name__ == "__main__":
+    search_replace()
+
