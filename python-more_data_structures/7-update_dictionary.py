e25055c9b65df99ea7e451b7ca23fadd90239774
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-18 16:01:14 -0800

    Task7MoreStruct

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,8 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
     return a_dictionary
-    
+
+
 def print_sorted_dictionary(a_dictionary):
     sorted_dicc = sorted(a_dictionary.items())
     for c, v in sorted_dicc:
