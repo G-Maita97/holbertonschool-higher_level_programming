5507feb0852597a874fa25bfa99686418102f93b
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-26 12:54:22 -0800

    MoreClass&ObjTask0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module 0-rectangle(class Rectangle)"""
+
+
+class Rectangle:
+    """empty class Rectangle.
+
+    """
+    pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,1 @@
+Contenido readme more class & obj
