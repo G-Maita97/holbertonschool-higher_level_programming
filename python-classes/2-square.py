8bbc998b5f4fff43a6f669fdb86a25cf90e8dbab
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-25 19:12:03 -0800

    Class&ObjTask2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,30 @@
 #!/usr/bin/python3
-class Square:
+"""Module 1-square.py(class Square)"""
 
-    def __init__(self, size=0): 
+
+class Square:
+    """" Class square represents the size of the geometric figure.
+
+    Attributes:
+        __size (int): Private instance attribute.
+
+    """
+    def __init__(self, size=0):
+        """__init__ method.
+
+        The __init__ method initializes the "size"
+        instance which will be the size of the square.
+
+        Args:
+            size (int): The size of the square (opcional value).
+
+        Raises:
+            TypeError: if size is not integer.
+            ValueError: if size is less than 0.
+
+        """
         if type(size) is not int:
             raise TypeError('size must be an integer')
-
         if size < 0:
             raise ValueError('size must be >= 0')
-
         self.__size = size
