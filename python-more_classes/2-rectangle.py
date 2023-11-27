2900b0a5621e1b73b9e61b6502c1dda7c432b70e
G-Maita97 <gustavoalternativox@gmail.com>
2023-11-26 16:39:16 -0800

    task2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -52,8 +52,9 @@ class Rectangle:
 
         Return:
             None
+
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('width must be an integer')
         if value < 0:
             raise ValueError('width must be >= 0')
@@ -86,7 +87,7 @@ class Rectangle:
             None
 
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError('height must be an integer')
         if value < 0:
             raise ValueError('height must be >= 0')
