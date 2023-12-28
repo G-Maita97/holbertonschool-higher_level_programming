03a5852cc393ef844b74b3fd9d61568c76625b04
G-Maita97 <gustavoalternativox@gmail.com>
2023-12-28 13:41:26 -0800

    task 1 mysql

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- CREATE DATABASE: Inicia el comando para crear una base de datos.
+-- IF NOT EXISTS: Es una cláusula que condicionalmente ejecuta la instrucción.
+-- Este comando crea la base de datos con el nombre especificado (nombre_de_tu_base_de_datos), pero solo si esa base de datos aún no existe. Si la base de datos ya existe, no se realizará ninguna acción y no se generará un error.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
