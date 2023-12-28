c168fe239f66db134674ce176c2376bf6cbcb18a
G-Maita97 <gustavoalternativox@gmail.com>
2023-12-28 15:56:21 -0800

    task 2 and UP task 1

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- DROP DATABASE: Inicia el comando para eliminar una base de datos.
 -- IF EXISTS: Es una cláusula que condicionalmente ejecuta la instrucción.
-DROP DATABASE IF EXISTS 'hbtn_0c_0';
+DROP DATABASE IF EXISTS hbtn_0c_0;
