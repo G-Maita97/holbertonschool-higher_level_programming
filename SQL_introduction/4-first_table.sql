8669708afd0aa65770bd1a78758627f625cd932f
G-Maita97 <gustavoalternativox@gmail.com>
2023-12-28 16:15:23 -0800

    task 4

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- id es una columna de tipo INT (entero), que se utiliza comúnmente para almacenar números enteros.
 -- name es una columna de tipo VARCHAR(256), que se utiliza para almacenar cadenas de texto de longitud variable con un máximo de 256 caracteres.
-CREATE TABLE IF NOT EXISTS 'first_table' (id INT, name 	VARCHAR(256));
+CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
