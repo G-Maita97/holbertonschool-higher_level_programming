4a39974316b9a5e8a0515f405848166122f8bc81
G-Maita97 <gustavoalternativox@gmail.com>
2023-12-29 11:57:57 -0800

    tas 15

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,7 +1,7 @@
--- SELECT score, COUNT(*) AS number``: Esta parte de la consulta selecciona dos columnas: score y la cuenta de las filas para cada valor único de score.
+-- SELECT score, COUNT(*) AS number: Esta parte de la consulta selecciona dos columnas: score y la cuenta de las filas para cada valor único de score.
 -- La función COUNT(*): cuenta el número de filas para cada valor único de score. El resultado de esta cuenta se le asigna un alias llamado number.
--- GROUP BY score``: Esta cláusula agrupa los resultados basándose en los valores únicos de la columna score. La función de agregación COUNT(*) se aplica a cada grupo de valores idénticos de score. 
-SELECT score, COUNT(*) AS 'number'
+-- GROUP BY score: Esta cláusula agrupa los resultados basándose en los valores únicos de la columna score. La función de agregación COUNT(*) se aplica a cada grupo de valores idénticos de score. 
+SELECT score, COUNT(*) AS number
 FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY number DESC;
