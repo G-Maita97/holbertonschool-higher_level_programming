0b57dcb5e3860c5e0fc54ec64361c03623f751ff
G-Maita97 <gustavoalternativox@gmail.com>
2023-12-29 11:35:44 -0800

    task 13

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,4 @@
+-- DELETE FROM second_table``: Esta parte de la instrucción indica que se van a eliminar filas de la tabla llamada second_table.
+-- WHERE score <= 5;: Esta cláusula especifica la condición que deben cumplir las filas para ser eliminadas. En este caso, la instrucción eliminará las filas donde el valor en la columna score sea menor o igual a 5.
+DELETE FROM second_table
+WHERE score <= 5;
