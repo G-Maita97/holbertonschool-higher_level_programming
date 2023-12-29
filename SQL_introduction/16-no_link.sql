939ee2aa53f5b2171e8921d2f39bbe902b706808
G-Maita97 <gustavoalternativox@gmail.com>
2023-12-29 12:15:30 -0800

    task 16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
--- 
+-- WHERE name IS NOT NULL: Esta cláusula filtra los resultados, seleccionando solo las filas donde el valor en la columna name no sea nulo. La expresión IS NOT NULL se utiliza para verificar si un valor no es nulo.
 SELECT score, name
 FROM  second_table
-WHERE name != NULL
+WHERE name IS NOT NULL
 ORDER BY score DESC;
